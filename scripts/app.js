9741ebfbb252a20bd0b6329a370472a67ed33568
atilla-arslan <atillaa@rocketmail.comm>
2021-05-18 11:35:20 +0100

    portoflio done

---
+++ scripts/app.js
@@ -0,0 +1,15 @@
+/* eslint-disable comma-dangle */
+/* eslint-disable no-undef */
+
+new fullpage('#fullpage', {
+  //options here
+  anchors: ['home', 'bio', 'tech', 'work', 'contact'],
+  autoScrolling: true,
+  scrollHorizontally: true,
+  navigation: true,
+  licenseKey: null,
+  paddingTop: '50px',
+  // verticalCentered: false,
+})
+//methods
+fullpage_api.setAllowScrolling(false)
