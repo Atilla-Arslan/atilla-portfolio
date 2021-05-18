5a0ab47024bd52b7d42dbb9ff5b7713dd9990752
atilla-arslan <atillaa@rocketmail.comm>
2021-05-18 17:57:31 +0100

    added project descriptions and scolling

--- scripts/app.js
+++ scripts/app.js
@@ -5,11 +5,12 @@ new fullpage('#fullpage', {
   //options here
   anchors: ['home', 'bio', 'tech', 'work', 'contact'],
   autoScrolling: true,
-  scrollHorizontally: true,
   navigation: true,
   licenseKey: null,
   paddingTop: '50px',
+  keyboardScrolling: true,
+  // continuousVertical: true,
   // verticalCentered: false,
 })
 //methods
-fullpage_api.setAllowScrolling(false)
+fullpage_api.setAllowScrolling(true)
